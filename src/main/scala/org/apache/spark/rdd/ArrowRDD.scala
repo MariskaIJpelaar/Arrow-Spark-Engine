c76c01dc850ed32feea5d25dab899489b4302110
Federico Fiorini <federico.fiorini@outlook.it>
2021-07-22 06:34:53 +0200

    latest version, still some exceptions in deserialization

--- src/main/scala/org/apache/spark/rdd/ArrowRDD.scala
+++ src/main/scala/org/apache/spark/rdd/ArrowRDD.scala
@@ -1,5 +1,7 @@
 package org.apache.spark.rdd
 
+import org.apache.arrow
+import org.apache.arrow.memory.{BufferAllocator, RootAllocator}
 import org.apache.arrow.vector._
 import org.apache.arrow.vector.types.Types.MinorType
 import org.apache.spark.{InterruptibleIterator, Partition, SparkContext, TaskContext}
@@ -19,9 +21,40 @@ import scala.collection.mutable.ArrayBuffer
 * and it uses Arrow-backed value vectors instead of the usual Scala Seq[T] collection */
 class ArrowPartition[T: ClassTag](var rddId: Long,
                                   var slice: Int,
-                                  var data: Seq[T]) extends Partition with Serializable{
+                                  var data: ValueVector) extends Partition with Externalizable {
 
-  def iterator : Iterator[T] = data.iterator
+  //Hack -deserialization requires a no-arg constructor for an ArrowPartition to work (NOT SURE)
+  def this() = {
+    this(0L, 0, new NullVector().asInstanceOf[ValueVector])
+  }
+
+  def iterator : Iterator[T] = {
+    var idx = 0
+
+    new Iterator[T] {
+      override def hasNext: Boolean = idx < data.getValueCount
+
+      override def next(): T = {
+        val vecType = data.getMinorType
+
+        vecType match {
+          case MinorType.BIGINT =>
+            val value = data.asInstanceOf[BigIntVector].get(idx)
+            idx = idx + 1
+
+            value.asInstanceOf[T]
+          case MinorType.VARBINARY =>
+            val dataBuf = data.asInstanceOf[BaseVariableWidthVector].getDataBuffer
+            val offBuf = data.asInstanceOf[BaseVariableWidthVector].getOffsetBuffer
+            val value = BaseVariableWidthVector.get(dataBuf, offBuf, idx)
+            idx = idx + 1
+
+            value.asInstanceOf[T]
+          case _ => throw new Exception("Unsupported vector type")
+        }
+      }
+    }
+  }
 
   // same as ParallelCollectionPartition[T]
   override def hashCode() : Int = (41 * (41 + rddId) + slice).toInt
@@ -34,37 +67,48 @@ class ArrowPartition[T: ClassTag](var rddId: Long,
   override def index: Int = slice
 
   @throws(classOf[IOException])
-  private def writeObject(out: ObjectOutputStream): Unit = Utils.tryOrIOException {
-
-    val sfactory = SparkEnv.get.serializer
-
-    // Treat java serializer with default action rather than going thru serialization, to avoid a
-    // separate serialization header.
-
-    sfactory match {
-      case js: JavaSerializer => out.defaultWriteObject()
-      case _ =>
-        out.writeLong(rddId)
-        out.writeInt(slice)
-
-        val ser = sfactory.newInstance()
-        Utils.serializeViaNestedStream(out, ser)(_.writeObject(data))
+  override def writeExternal(out: ObjectOutput): Unit = {
+    println("Begin writeExternal")
+    out.writeLong(rddId)
+    out.writeInt(slice)
+    out.writeObject(data.getMinorType)
+    for (i <- 0 until data.getValueCount-1) {
+      out.writeObject(data.getObject(i))
     }
+    println("End writeExternal")
   }
 
-  @throws(classOf[IOException])
-  private def readObject(in: ObjectInputStream): Unit = Utils.tryOrIOException {
+  override def readExternal(in: ObjectInput): Unit = {
+    println("Begin readExternal")
+    rddId = in.readLong()
+    slice = in.readInt()
+    val minorType = in.readObject().asInstanceOf[MinorType]
 
-    val sfactory = SparkEnv.get.serializer
-    sfactory match {
-      case js: JavaSerializer => in.defaultReadObject()
-      case _ =>
-        rddId = in.readLong()
-        slice = in.readInt()
+    val allocator : BufferAllocator = new RootAllocator(Long.MaxValue)
+    minorType match {
+      case MinorType.BIGINT => {
+        var vector = new BigIntVector("vector", allocator)
+        var index = 0
+        while (in.available() > 0){
+          vector.setSafe(index, in.readLong())
+          index+=1
+        }
 
-        val ser = sfactory.newInstance()
-        Utils.deserializeViaNestedStream(in, ser)(ds => data = ds.readObject[Seq[T]]())
+        data = vector.asInstanceOf[ValueVector]
+      }
+      case MinorType.VARBINARY => {
+        var vector = new VarCharVector("vector", allocator)
+        var index = 0
+        while (in.available() > 0){
+          vector.setSafe(index, in.readObject().asInstanceOf[Array[Byte]])
+          index+=1
+        }
+
+        data = vector.asInstanceOf[ValueVector]
+      }
+      case _ => throw new InvalidClassException("No valid type")
     }
+    println("End readExternal")
   }
 }
 
@@ -80,8 +124,8 @@ class ArrowRDD[T: ClassTag](sc: SparkContext,
   }
 
   override protected def getPartitions: Array[Partition] = {
-    val values = ArrowRDD.vectorExtractor(data)
-    val slices = ArrowRDD.slice[T](values, numSlices).toArray
+//    val values = ArrowRDD.vectorExtractor(data)
+    val slices = ArrowRDD.slice[T](data, numSlices).toArray
     slices.indices.map(i => new ArrowPartition(id, i, slices(i))).toArray
   }
 
@@ -92,97 +136,48 @@ class ArrowRDD[T: ClassTag](sc: SparkContext,
 
 private object ArrowRDD {
 
-  def slice[T: ClassTag](seq: Seq[T],
-                         numSlices: Int) : Seq[Seq[T]] = {
+  def slice[T: ClassTag](vec: ValueVector,
+                         numSlices: Int): Seq[ValueVector] = {
 
     if (numSlices < 1) throw new IllegalArgumentException("Positive number of partitions required")
 
-    def positions(length: Long, numSlices: Int) : Iterator[(Int, Int)] = {
+    def positions(length: Long, numSlices: Int): Iterator[(Int, Int)] = {
       (0 until numSlices).iterator.map { i =>
         val start = ((i * length) / numSlices).toInt
-        val end   = (((i + 1) * length) / numSlices).toInt
+        val end = (((i + 1) * length) / numSlices).toInt
 
-        (start,end)
+        (start, end)
       }
     }
+
     /**
      * Slice original ValueVector with zero-copy, using the Slicing operations described
      * in the Java Arrow API documentation page.
      * Then retrieve the data from each individual slice and convert it to Seq[T]
      */
-//    positions(vec.getValueCount, numSlices).map {
-//      case (start, end) => {
-//        val allocator = vec.getAllocator
-//        val tp        = vec.getTransferPair(allocator)
-//
-//        tp.splitAndTransfer(start, end)
-//        val splitVector = tp.getTo
-//
-//        (0 until end).iterator.map {
-//          i => splitVector.getObject(i).asInstanceOf[T]
-//        }.toSeq
-//      }
-//    }.toSeq
-//    val values = (0 until vec.getValueCount).iterator.map(i =>
-//          vec.getObject(i).asInstanceOf[T]).toSeq
-//
-//
-//    positions(values.length, numSlices).map {
-//      case (start, end) => values.slice(start, end)
-//    }.toSeq
-//    vectorExtractor[T](vec)
-//    positions(values.length, numSlices).map {
-//      case (start, end) => values.slice(start, end)
-//    }.toSeq
-    seq match {
-//      case r: Range =>
-//        positions(r.length, numSlices).zipWithIndex.map { case ((start, end), index) =>
-//          // If the range is inclusive, use inclusive range for the last slice
-//          if (r.isInclusive && index == numSlices - 1) {
-//            new Range.Inclusive(r.start + start * r.step, r.end, r.step)
-//          } else {
-//            new Range.Inclusive(r.start + start * r.step, r.start + (end - 1) * r.step, r.step)
-//          }
-//        }.toSeq.asInstanceOf[Seq[Seq[T]]]
-//      case nr: NumericRange[T] =>
-//        // For ranges of Long, Double, BigInteger, etc
-//        val slices = new ArrayBuffer[Seq[T]](numSlices)
-//        var r = nr
-//        for ((start, end) <- positions(nr.length, numSlices)) {
-//          val sliceSize = end - start
-//          slices += r.take(sliceSize).asInstanceOf[Seq[T]]
-//          r = r.drop(sliceSize)
-//        }
-//        slices.toSeq
-      case _ =>
-        val array = seq.toArray // To prevent O(n^2) operations for List etc
-        positions(array.length, numSlices).map { case (start, end) =>
-          array.slice(start, end).toSeq
-        }.toSeq
-    }
-  }
+    //    positions(vec.getValueCount, numSlices).map {
+    //      case (start, end) => {
+    //        val allocator = vec.getAllocator
+    //        val tp = vec.getTransferPair(allocator)
+    //
+    //        tp.splitAndTransfer(start, end)
+    //        val splitVector = tp.getTo
+    //
+    //        (0 until end).iterator.map {
+    //          i => splitVector.getObject(i).asInstanceOf[T]
+    //        }.toSeq
+    //      }
+    //    }
+    positions(vec.getValueCount, numSlices).map {
+      case (start, end) => {
+        val allocator = vec.getAllocator
+        val tp = vec.getTransferPair(allocator)
 
-   def vectorExtractor[T: ClassTag](vector: ValueVector) : Seq[T] = {
-    val vectorType = vector.getMinorType
-
-    vectorType match {
-      case MinorType.BIGINT => {
-        (0 until vector.getValueCount).iterator.map {
-          i => vector.asInstanceOf[BigIntVector].get(i).asInstanceOf[T]
-        }.toSeq
+        println(start +" " +end+ " indexes. Count: " +vec.getValueCount)
+        tp.splitAndTransfer(start, end-start)
+        tp.getTo
       }
-      case MinorType.VARBINARY => {
-        (0 until vector.getValueCount).iterator.map {
-          i => {
-            val dataBuf = vector.asInstanceOf[BaseVariableWidthVector].getDataBuffer
-            val offBuf = vector.asInstanceOf[BaseVariableWidthVector].getOffsetBuffer
-            BaseVariableWidthVector.get(dataBuf, offBuf, i).asInstanceOf[T]
-          }
-        }.toSeq
-      }
-      case _ => throw new Exception("Unsupported minor type")
-    }
+    }.toSeq
   }
-
 }
 
