c69e4156a4d25178a3e2cbbe89dfaa18093c8ea8
MariskaIJpelaar <m.m.j.ijpelaar@gmail.com>
2022-03-22 19:04:44 +0100

    WIP: getting the evaluator to work. Writing tests. Will not compile atm.

--- src/test/java/SmallParquetReaderTest.java
+++ src/test/java/SmallParquetReaderTest.java
@@ -1,6 +1,5 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.apache.arrow.parquet.ParquetToArrowConverter;
+import org.apache.arrow.vector.*;
 import org.apache.avro.Schema;
 import org.apache.parquet.hadoop.ParquetFileReader;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
@@ -8,12 +7,18 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.hadoop.util.HadoopOutputFile;
 import org.apache.parquet.io.OutputFile;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.MessageTypeParser;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utils.ParquetWriter;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Tutorials which got me started: (2022-03-18)
@@ -28,19 +33,34 @@ public class SmallParquetReaderTest {
 
     @BeforeAll
     static void generateParquet() throws IOException {
+        File file = new File(filename);
+        assert !file.exists() || file.delete();
         ParquetWriter.write_default_simple(HadoopOutputFile.fromPath(new Path(filename), new Configuration()));
     }
 
 
     @Test
-    @DisplayName("First test")
-    void test() throws IOException {
+    @DisplayName("Test a simple file")
+    void simple_test() throws Exception {
         HadoopInputFile inputFile = HadoopInputFile.fromPath(new Path(filename), new Configuration());
         ParquetFileReader reader = ParquetFileReader.open(inputFile);
-        Schema other = ParquetWriter.get_default_schema();
-//        Schema own = reader.getFileMetaData().getSchema();
+        MessageType other = ParquetWriter.get_message_type();
+        assertNotNull(other);
+        MessageType own = reader.getFileMetaData().getSchema();
+        assertEquals(0, own.toString().compareTo(other.toString()));
 
-        //TODO: compare them with assertEquals
+        ParquetToArrowConverter handler = new ParquetToArrowConverter();
+        handler.process(filename);
+        VectorSchemaRoot root = handler.getVectorSchemaRoot();
+        for (ValueVector v: root.getFieldVectors()) {
 
+        }
+
+//        Optional<IntVector> ids = handler.getIntVector();
+//
+//        assertFalse(ids.isEmpty());
+//        Optional<VarCharVector> names = handler.getVarCharVector();
+//        assertFalse(names.isEmpty());
+//        assert false;
     }
 }
--- src/test/java/utils/ParquetWriter.java
+++ src/test/java/utils/ParquetWriter.java
@@ -1,15 +1,25 @@
 package utils;
 
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.FieldVector;
+import org.apache.arrow.vector.IntVector;
+import org.apache.arrow.vector.VarCharVector;
+import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.parquet.arrow.schema.SchemaConverter;
+import org.apache.parquet.arrow.schema.SchemaMapping;
 import org.apache.parquet.avro.AvroParquetWriter;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.apache.parquet.io.OutputFile;
+import org.apache.parquet.schema.MessageType;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 // from: https://stackoverflow.com/a/39734610 (2022-03-18)
@@ -17,18 +27,56 @@ import java.util.List;
 
 public class ParquetWriter {
     private static final Schema default_schema = SchemaBuilder.builder("simple").record("record")
-            .fields().requiredInt("id").requiredBoolean("gender_male").requiredString("name")
+            .fields().requiredInt("id").requiredString("name")
             .endRecord();
-
     public static Schema get_default_schema() { return default_schema; }
+
+    private static MessageType message_type = null;
+    public static MessageType get_message_type() { return message_type; }
+
+    private static List<FieldVector> fieldVectors = null;
 
     public static void write_default_simple(OutputFile fileToWrite) {
         List<GenericData.Record> recordsToWrite = List.of(
-                new GenericRecordBuilder(default_schema).set("id", 1).set("gender_male", true).set("name", "John").build(),
-                new GenericRecordBuilder(default_schema).set("id", 2).set("gender_male", false).set("name", "Suzie").build(),
-                new GenericRecordBuilder(default_schema).set("id", 3).set("gender_male", true).set("name", "Peter").build()
+                new GenericRecordBuilder(default_schema).set("id", 1).set("name", "John").build(),
+                new GenericRecordBuilder(default_schema).set("id", 2).set("name", "Suzie").build(),
+                new GenericRecordBuilder(default_schema).set("id", 3).set("name", "Peter").build()
         );
         write(fileToWrite, default_schema, recordsToWrite);
+    }
+
+    private static RuntimeException setFieldVectors(Schema schema, List<GenericData.Record> recordsToWrite) {
+        // TODO: implement!! Might be nonsense now...
+        BufferAllocator allocater = new RootAllocator(Integer.MAX_VALUE);
+        SchemaConverter converter = new SchemaConverter();
+        SchemaMapping mapping = converter.fromParquet(get_message_type());
+        org.apache.arrow.vector.types.pojo.Schema arrowSchema = mapping.getArrowSchema();
+
+        fieldVectors = new ArrayList<>();
+        for (Field field : arrowSchema.getFields()) {
+//            Schema.Type type = field.schema().getType();
+            FieldVector vector = field.createVector(allocater);
+
+            switch (field.getType().getTypeID()) {
+                case Int: ((IntVector) vector).allocateNew(); break;
+                case Binary: ((VarCharVector) vector).allocateNew(); break;
+                default: return new RuntimeException("[ParquetWriter] type not supported");
+            }
+
+            int i = 0;
+            for (GenericData.Record record : recordsToWrite) {
+                switch (field.getType().getTypeID()) {
+                    case Int: ((IntVector) vector).setSafe();
+                        break;
+                    case Binary:
+                        break;
+                    default: return new RuntimeException("[ParquetWriter] type not supported");
+                }
+                ++i;
+            }
+            fieldVectors.add(vector);
+        }
+        return null;
     }
 
     public static void write(OutputFile fileToWrite, Schema schema, List<GenericData.Record> recordsToWrite) {
@@ -41,9 +89,12 @@ public class ParquetWriter {
             for (GenericData.Record record : recordsToWrite) {
                 writer.write(record);
             }
+            writer.close(); // so we can get the Footer
+            message_type = writer.getFooter().getFileMetaData().getSchema();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        setFieldVectors(schema, recordsToWrite);
     }
 
 
