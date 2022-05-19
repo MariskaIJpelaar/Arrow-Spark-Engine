85d7e4d4328f73239dd69f738fadceb87d64c070
MariskaIJpelaar <m.m.j.ijpelaar@gmail.com>
2022-05-19 18:10:35 +0200

    WIP: debugging prints

--- sparrow/src/main/java/org/apache/arrow/parquet/ParquetToArrowConverter.java
+++ sparrow/src/main/java/org/apache/arrow/parquet/ParquetToArrowConverter.java
@@ -17,6 +17,7 @@
 
 package org.apache.arrow.parquet;
 
+import io.netty.util.internal.PlatformDependent;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.parquet.utils.DumpGroupConverter;
 import org.apache.arrow.vector.*;
@@ -121,16 +122,24 @@ public class ParquetToArrowConverter {
   public void process(Directory dir) throws IOException {
     t2 = System.nanoTime();
 
+    System.out.println("A" + PlatformDependent.usedDirectMemory());
+
     files = dir.files().filter((file) -> Objects.equals(FilenameUtils.getExtension(file.name()), "parquet")).toList();
 
+    System.out.println("B" + PlatformDependent.usedDirectMemory());
+
     files.foreach( (file) -> {
+      System.out.println("C" + PlatformDependent.usedDirectMemory());
       try {
         HadoopInputFile inputFile = HadoopInputFile.fromPath(new Path(file.path()), configuration);
+        System.out.println("CX" + PlatformDependent.usedDirectMemory());
         ParquetFileReader reader = ParquetFileReader.open(inputFile);
+        System.out.println("CY" + PlatformDependent.usedDirectMemory());
         if (parquetSchema == null)
           parquetSchema = reader.getFileMetaData().getSchema();
         else
           parquetSchema.union(reader.getFileMetaData().getSchema());
+        System.out.println("CZ" + PlatformDependent.usedDirectMemory());
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
@@ -138,12 +147,17 @@ public class ParquetToArrowConverter {
     });
     t3 = System.nanoTime();
     time.add((t3 - t2) / 1e9d);
+    System.out.println("D" + PlatformDependent.usedDirectMemory());
 
     t4 = System.nanoTime();
     SchemaConverter converter = new SchemaConverter();
+    System.out.println("E" + PlatformDependent.usedDirectMemory());
     SchemaMapping mapping = converter.fromParquet(parquetSchema);
+    System.out.println("F" + PlatformDependent.usedDirectMemory());
     arrowSchema = mapping.getArrowSchema();
+    System.out.println("G" + PlatformDependent.usedDirectMemory());
     vectorSchemaRoot = VectorSchemaRoot.create(arrowSchema, allocator);
+    System.out.println("H" + PlatformDependent.usedDirectMemory());
     rowsCount = new ArrayList<>(Collections.nCopies(vectorSchemaRoot.getFieldVectors().size(), 0));
     t5 = System.nanoTime();
     time.add((t5 - t4) / 1e9d);
@@ -151,9 +165,13 @@ public class ParquetToArrowConverter {
     final int[] offset = {0}; // Array because it is passed by reference, not by value
     int totalRows = files.mapConserve((file) -> {
       try {
+        System.out.println("IA" + PlatformDependent.usedDirectMemory());
         HadoopInputFile inputFile = HadoopInputFile.fromPath(new Path(file.path()), configuration);
+        System.out.println("IB" + PlatformDependent.usedDirectMemory());
         ParquetFileReader reader = ParquetFileReader.open(inputFile);
+        System.out.println("IC" + PlatformDependent.usedDirectMemory());
         int ret = Trivedi(reader, offset[0]);
+        System.out.println("ID" + PlatformDependent.usedDirectMemory());
         offset[0] += ret;
         return ret;
       } catch (Exception e) {
@@ -161,8 +179,11 @@ public class ParquetToArrowConverter {
       }
     }).toStream().reduceOption(Integer::sum).getOrElse(() -> -1);
 
+    System.out.println("J" + PlatformDependent.usedDirectMemory());
     setVectors();
+    System.out.println("K" + PlatformDependent.usedDirectMemory());
     vectorSchemaRoot.setRowCount(totalRows);
+    System.out.println("L" + PlatformDependent.usedDirectMemory());
     t7 = System.nanoTime();
     if (t6 != null)
       time.add((t7 - t6) / 1e9d);
