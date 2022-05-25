114b2342a9f91acec468f2f6c6da6d64611f8a2d
MariskaIJpelaar <m.m.j.ijpelaar@gmail.com>
2022-05-25 13:10:31 +0200

    ArrowSparkContext can now cast a 'normal RDD' to an ArrowRDD[T]

--- sparrow/src/main/scala/org/apache/spark/ArrowSparkContext.scala
+++ sparrow/src/main/scala/org/apache/spark/ArrowSparkContext.scala
@@ -1,7 +1,7 @@
 package org.apache.spark
 
 import org.apache.arrow.vector.ValueVector
-import org.apache.spark.rdd.ArrowRDD
+import org.apache.spark.rdd.{ArrowRDD, RDD}
 
 import scala.reflect.ClassTag
 import scala.reflect.runtime.universe._
@@ -17,5 +17,17 @@ class ArrowSparkContext(config: SparkConf) extends SparkContext(config) {
                                 numSlices: Int = defaultParallelism): ArrowRDD[T] = withScope {
     assertNotStopped()
     new ArrowRDD[T](this, vectors, numSlices, Map[Int, Seq[String]]())
+  }
+
+  /**
+   * Methods to 'cast' an RDD[T] back to an ArrowRDD[T]
+   * Assumes the underlying RDD[T] is actually an ArrowRDD[T]
+   * @param rdd the RDD to cast
+   * @tparam T the type saved in the RDD
+   * @return the rdd as an Instance of ArrowRDD[T]
+   */
+  def makeArrowRDD[T: ClassTag : TypeTag](rdd: RDD[T]): ArrowRDD[T] = withScope {
+    assertNotStopped()
+    rdd.asInstanceOf[ArrowRDD[T]]
   }
 }
